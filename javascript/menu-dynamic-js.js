d6a1cd1dc9b83e0d0714c0aaa116b7bfc2c7ba20
HOSTNAME47 <mouadstev1@gmail.com>
2020-09-13 21:37:06 +0100

    make menu dynamic and add search field

--- javascript/menu-dynamic-js.js
+++ javascript/menu-dynamic-js.js
@@ -6,3 +6,15 @@ $("#mobile-logo").on("mouseup", function() {
     console.log("mouse up")
     $("#mobile-logo").css("color", "white");
 });
+
+$("#nav-button").click(function() {
+    let navContainer = $("#mobile-nav");
+
+    if(navContainer.css("max-height") == "0px") {
+        console.log("test");
+        navContainer.css("max-height", "500px")
+    }
+    else {
+        navContainer.css("max-height", "0px")
+    }
+})
